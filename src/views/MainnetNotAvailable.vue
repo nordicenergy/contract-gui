9ec6add648b175eb628715f87f5d2fdbbf558243
MichalGallovic <gallovicm@gmail.com>
2019-11-12 21:34:39 +0100

    Update smartcontract to a href

--- src/views/MainnetNotAvailable.vue
+++ src/views/MainnetNotAvailable.vue
@@ -7,7 +7,9 @@
         <h1 class="font-lition text-4xl font-bold text-white">Side Chain Manager</h1>
       </div>
       <p class="text-xl text-white leading-loose text-center">
-        GUI is currently not compatible with the relaunched mainnet. For interaction with the Blockchain, please interact manually with the Smart Contract
+        GUI is currently not compatible with the relaunched mainnet. For interaction with the Blockchain, please
+        interact manually with the <a class="text-active hover:underline" href="https://etherscan.io/address/0x7a79868b8375131B4c6A681b112109A51EEa0a6C"
+                                      target="_blank">Smart Contract</a>.
       </p>
     </div>
   </main>
