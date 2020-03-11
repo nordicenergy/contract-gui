69bff55b0aa72547e6aa2fbe3c567902ab4102b7
Nordic Energy Dev <54575925+nordicenergy@users.noreply.github.com>
2020-03-11 14:11:05 +0200

    Update MainnetNotAvailable.vue

--- src/views/MainnetNotAvailable.vue
+++ src/views/MainnetNotAvailable.vue
@@ -7,10 +7,10 @@
         <h2 class="font-nordicenergy text-4xl font-bold text-white">Side Chain Manager</h2>
       </div>
       <p class="text-xl text-white leading-loose text-center">
-        To withdraw Nordic Energy tokens from old Smart Contract use <a class="text-active hover:underline" href="https://nordicenergy.io/sidechainmanagerv1" target="_blank">https://nordicenergy.io/sidechainmanagerv1</a>
+        To withdraw Nordic Energy tokens from old Smart Contract use <a class="text-active hover:underline" href="https://nodemanager.nordicenergy.io/sidechainmanagerv1" target="_blank">https://nodemanager.nordicenergy.io/sidechainmanagerv1</a>
       </p>
       <p class="text-xl text-white leading-loose text-center">
-        To interact with current Smart Contract use <a class="text-active hover:underline" href="https://nordicenergy.io/sidechainmanagerv2" target="_blank">https://nordicenergy.io/sidechainmanagerv2</a>
+        To interact with current Smart Contract use <a class="text-active hover:underline" href="https://nodemanager.nordicenergy.io/sidechainmanagerv2" target="_blank">https://nodemanager.nordicenergy.io/sidechainmanagerv2</a>
       </p>
     </div>
   </main>
